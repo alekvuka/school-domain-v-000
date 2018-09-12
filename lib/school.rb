50f7661e233b69f062e0cb41768d24e32a678404
ALEKSANDAR VUKASINOVIC <alekvuka@gmail.com>
2018-09-12 23:27:18 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,24 +9,10 @@ class School
     @roster = Hash.new
   end
 
-  
 
   def add_student(name, grade)
 
-
-      @roster[grade] = name
-
-
-      #ROSTER.each do |gr, nm|
-      #  if gr == grade
-      #    nm.push(name)
-      #    return ROSTER
-      #  end
-      #end
-
-    #else
-    #  ROSTER[grade] = name
-    #end
+    @roster[grade] = name
 
   end
 
